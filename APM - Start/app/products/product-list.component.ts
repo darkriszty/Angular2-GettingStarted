f0dc4bcb39c95e50d557497ce4a0755fe39c1bb4
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-02-11 20:47:08 +0200

    Add 2 way binding for filter and simple pipes.

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -9,6 +9,7 @@ export class ProductListComponent {
     imageWidth: number = 50;
     imageMargin: number = 2;
     imageShown: boolean = false;
+    listFilter: string = "cart";
     products: any[] = [
         {
             "productId": 1,
