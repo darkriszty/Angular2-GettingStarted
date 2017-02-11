650446449bf4f573b6bae0f225e5a632ce179227
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-02-11 19:47:40 +0200

    Display the product list component on the page.

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ProductListComponent } from './products/product-list.component';
+
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <h1>Acme Product Management</h1>
+        <div>
+            <pm-products></pm-products>
+        </div>
     `
 })
 export class AppComponent { }
